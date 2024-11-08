32cb4118cfaf465829da41f4a3eeee10ed06e855
crystel1996 <crystel.ratsimbazafy@swanforlife.com>
2024-11-08 17:11:58 +0300

    add color

---
+++ src/utils/color.ts
@@ -0,0 +1,14 @@
+const COLOR = {
+    earthBlue: '#2E3A59',
+    globalGreen: '#4CAF50',
+    sunsetOrange: '#FF7043',
+    saharaBeige: '#F5E1A4',
+    oceanTeal: '#009688',
+    goldenYellow: '#FFEB3B',
+    softCoral: '#FF8A65',
+    cloudWhite: '#FAFAFA',
+    stoneGray: '#BDBDBD',
+    slateBlack: '#2C3E50'
+}
+
+export default COLOR;
